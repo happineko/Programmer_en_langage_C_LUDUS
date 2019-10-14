bcc1de8c5eddacc7def6a0a1853e528a0c9b744d
HappiNeko <y.bonin@ludus-academie.com>
2019-10-14 17:23:36 +0200

    Programmation en langage C, Programme racine carrées

--- Programmer_en_langage_C_YB_V1_0/main.c
+++ Programmer_en_langage_C_YB_V1_0/main.c
@@ -1,8 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> /**< librairie de périphérique d'entrée/sortie */
+#include <stdlib.h> /**< Bibliothèque de méthodes basiques (timer random ...) */
+#include <math.h>
+
+#define NFOIS 5
 
 int main()
 {
+    int i;
+    float x;
+    float racx;
+
     printf("Hello world!\n");
+
+    printf("je vais vous calculer %d racines carrées\n",NFOIS);
+
+    for(i=0;i<NFOIS;i++){
+        printf("donnez un nombre : ");
+        scanf("%f",&x);
+        if(x<0.0)
+            printf("le nombre %f ne possède pas de racine carrée\n",x);
+        else{
+            racx=sqrt(x);
+            printf("le nombre %f a pour racine carrée : %f\n",x,racx);
+        }
+    }
+
+    printf("travail terminé - Au revoir");
+
     return 0;
+
 }
