790304525f938f39215d5a1d8238a596a3b58e18
HappiNeko <y.bonin@ludus-academie.com>
2019-10-15 10:57:07 +0200

    Programmer en langage C - suite

--- Programmer_en_langage_C_YB_V1_0/main.c
+++ Programmer_en_langage_C_YB_V1_0/main.c
@@ -2,9 +2,12 @@
 #include <stdlib.h> /**< Bibliothèque de méthodes basiques (timer random ...) */
 #include <math.h>
 
-#define NFOIS 5
+#define NFOIS 5 //constante
+#define PI 3.14
+#define Nom "moi"
+#define CAR 'B'
 
-int main()
+/* int main() //Calcul racine carrees
 {
     int i;
     float x;
@@ -28,5 +31,71 @@ int main()
     printf("travail terminé - Au revoir");
 
     return 0;
+
+} */
+
+int main() //Programme sur les types
+{
+    //Integer
+    short int nA;
+    int nB;
+    long int nC;
+
+    printf("taille de a : %d octet(s)\n", sizeof(nA));
+    printf("taille de b : %d octet(s)\n", sizeof(nB));
+    printf("taille de c : %d octet(s)\n", sizeof(nC));
+
+    //Reel
+    float fltReelSimple;
+    double dblReelDouble;
+
+    printf("taille de fltReelSimple : %d octet(s)\n",sizeof(fltReelSimple));
+    printf("taille de dblReelDouble : %d octet(s)\n",sizeof(dblReelDouble));
+
+    //Caractere
+    char cCaractere;
+    printf("taille de cCaractere : %d octet(s)\n",sizeof(cCaractere));
+
+    //Test Define
+    printf("affichage de PI : %f\n",PI);
+    printf("affichage de nom : %s\n",Nom);
+    printf("affichage de caractere : %c\n",CAR);
+
+    //Opérateurs
+    int nI=0;
+    ++nI; //pre-incrementation
+    nI++; //post-incrementation
+    //% modulo
+    // / division (attention aux types)
+
+    //Cast
+    long double ldblLongDouble=105.55519841119745121284;
+    printf("valeur du long double : %lf\n",(double)ldblLongDouble);
+
+    //Operateurs relationnels
+    // >
+    // <
+    // >=
+    // <=
+    // ==
+    // !=
+
+    //operateurs logiques
+    // & AND
+    // &&
+    // | OR
+    // ||
+    // ! NOT
+
+    //Factorisation opérateur
+    // +=
+    // -=
+    // *=
+    // /=
+    // %=
+
+    //boucle pour, tant que, faire tant que
+    // operateur ternaire
+
 
 }
