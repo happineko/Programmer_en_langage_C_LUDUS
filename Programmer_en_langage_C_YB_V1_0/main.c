bf1d65cc06cf570b97c65bc5ac4ccc24f34bcb67
HappiNeko <y.bonin@ludus-academie.com>
2019-10-21 15:53:29 +0200

    Apprentissage Programmation en C - Entrées Sortie conversationnel

--- Programmer_en_langage_C_YB_V1_0/main.c
+++ Programmer_en_langage_C_YB_V1_0/main.c
@@ -2,160 +2,28 @@
 #include <stdlib.h> /**< Bibliothèque de méthodes basiques (timer random ...) */
 #include <math.h>
 
-#define NFOIS 5 //constante
-#define PI 3.14
-#define Nom "moi"
-#define CAR 'B'
-
 #define TAILLE 10
+#define MOT "HELLO"
 
-/* int main() //Calcul racine carrees
+//Les entrees sorties conversationnels
+
+int main()
 {
-    int i;
-    float x;
-    float racx;
+    //Affichage
+    printf("%d\n",TAILLE);
+    printf("%s\n",MOT);
 
-    printf("Hello world!\n");
+    //Saisie
 
-    printf("je vais vous calculer %d racines carrées\n",NFOIS);
-
-    for(i=0;i<NFOIS;i++){
-        printf("donnez un nombre : ");
-        scanf("%f",&x);
-        if(x<0.0)
-            printf("le nombre %f ne possède pas de racine carrée\n",x);
-        else{
-            racx=sqrt(x);
-            printf("le nombre %f a pour racine carrée : %f\n",x,racx);
-        }
-    }
-
-    printf("travail terminé - Au revoir");
+    int val;
+    scanf("%d",&val);
+    fflush(stdin);
+    char car;
+    scanf("%c\n",&car);
+    fflush(stdin);
+    char tabCar[TAILLE];
+    scanf("%s",tabCar);
 
     return 0;
 
-} */
-
-int main() //Programme sur les types
-{
-    //Integer
-    short int nA;
-    int nB;
-    long int nC;
-
-    printf("taille de a : %d octet(s)\n", sizeof(nA));
-    printf("taille de b : %d octet(s)\n", sizeof(nB));
-    printf("taille de c : %d octet(s)\n", sizeof(nC));
-
-    //Reel
-    float fltReelSimple;
-    double dblReelDouble;
-
-    printf("taille de fltReelSimple : %d octet(s)\n",sizeof(fltReelSimple));
-    printf("taille de dblReelDouble : %d octet(s)\n",sizeof(dblReelDouble));
-
-    //Caractere
-    char cCaractere;
-    printf("taille de cCaractere : %d octet(s)\n",sizeof(cCaractere));
-
-    //Test Define
-    printf("affichage de PI : %f\n",PI);
-    printf("affichage de nom : %s\n",Nom);
-    printf("affichage de caractere : %c\n",CAR);
-
-    //Opérateurs
-    int nI=0;
-    ++nI; //pre-incrementation
-    nI++; //post-incrementation
-    //% modulo
-    // / division (attention aux types)
-
-    //Cast
-    long double ldblLongDouble=105.55519841119745121284;
-    printf("valeur du long double : %lf\n",(double)ldblLongDouble);
-
-    //Operateurs relationnels
-    // >
-    // <
-    // >=
-    // <=
-    // ==
-    // !=
-
-    //operateurs logiques
-    // & AND
-    // &&
-    // | OR
-    // ||
-    // ! NOT
-
-    //Factorisation opérateur
-    // +=
-    // -=
-    // *=
-    // /=
-    // %=
-
-    //boucle pour, tant que, faire tant que
-    // operateur ternaire
-
-    //Structures conditions et boucles
-    int a=0,b=0,c=0,d=0;
-
-    //condition
-    if(a==b){
-        printf("ils sont egaux \n");
-    }else{
-        printf("ils sont differents \n");
-    }
-
-    //conditions avec operateurs logiques
-
-    if((a==b)&&(a==c)){
-        printf("ils sont tous egaux\n");
-    }else{
-        printf("ils sont differents\n");
-    }
-    if((a==b)||(a==c)){
-        printf("ils sont tous egaux\n");
-    }else if((a==c)&&(a!=d)){
-        printf("blabla...\n");
-    }
-
-    //Boucle
-    int i=0;
-
-    for(i=0;i<TAILLE;i++){
-        printf("Hello World\n");
-    }
-
-    //tant que
-
-    while(i<TAILLE){
-        printf("Hello World\n");
-        i++;
-    }
-
-    //Faire tant que
-    i=0;
-    do{
-        printf("hello world\n");
-        i++;
-    }while(i<TAILLE);
-
-    //Cas alternatif de la condition
-    int choix=-1;
-
-    printf("Choisissez\n");
-    scanf("%d",&choix);
-    switch(choix){
-        case 0:printf("Quitter\n");break;
-        case 1:printf("Calculer\n");break;
-        case 2:printf("Jouer\n");break;
-        default:printf("Default\n");break;
-    }
-
-    //Test ? instruction 1:Instruction 2
-
-    (a==b)? printf("Egaux"):printf("different");
 }
