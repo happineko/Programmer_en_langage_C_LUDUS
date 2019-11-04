b7e1924e4283dde353b6a9d8e60c686f46552cd6
HappiNeko <y.bonin@ludus-academie.com>
2019-11-04 14:20:50 +0100

    Tp Morpion (Avec quelques problemes)

---
+++ main.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAILLE 4
+
+void initTab(char grille[TAILLE][TAILLE]);
+void affTab(char grille[TAILLE][TAILLE]);
+int verifWTL(char grille[TAILLE][TAILLE]);
+int verifOOBX(int newX);
+int verifOOBY(int newY);
+//int verifOccPlace(char grille[TAILLE][TAILLE], int newX, int newY);
+
+int main()
+{
+    int newX=0, newY=0;
+    char grille[TAILLE][TAILLE];
+    int joueur = 1;
+    int tour = 0;
+    initTab(grille);
+    affTab(grille);
+    while((verifWTL(grille)== 0)&&(tour<=9)){
+        if(joueur == 1){
+            tour += 1;
+           // while(verifOccPlace(grille, newX, newY)== 1){
+                printf("Ou voulez-vous placer le X ?\n");
+                printf("X : ");
+                scanf("%d",&newX);
+                verifOOBX(newX);
+                fflush(stdin);
+                printf("Y : ");
+                scanf("%d",&newY);
+                fflush(stdin);
+                verifOOBY(newY);
+           // }
+            grille[newY][newX]='X';
+            joueur = 2;
+        }else{
+            tour += 1;
+           // while(verifOccPlace(grille, newX, newY)== 1){
+                printf("Ou voulez-vous placer le O ?\n");
+                printf("X : ");
+                scanf("%d",&newX);
+                fflush(stdin);
+                verifOOBX(newX);
+                printf("Y : ");
+                scanf("%d",&newY);
+                fflush(stdin);
+                verifOOBY(newY);
+           // }
+            grille[newY][newX]='O';
+            joueur = 1;
+        }
+
+        affTab(grille);
+    }
+    printf("GAME OVER");
+    return 0;
+}
+
+//Procedure d'initialisation de la grille
+void initTab(char grille[TAILLE][TAILLE]){
+    int i,j;
+    for(i=1;i<=TAILLE;i++){
+        for(j=1;j<=TAILLE;j++){
+            grille[i][j]='_';
+        }
+    }
+}
+
+//Procedure d'affichage de la grille
+void affTab(char grille[TAILLE][TAILLE]){
+    int i,j;
+    for(i=1;i<=3;i++){
+        for(j=1;j<=3;j++){
+            printf("%c",grille[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+//Verification condition win/tie/lose
+int verifWTL(char grille[TAILLE][TAILLE]){
+    int i,j;
+    int finish = 0;
+    int comptX, comptO;
+
+    //Verification pour le caractere x et o en ligne
+    if(finish == 0){
+        for(i=1;i<=TAILLE;i++){
+            comptX = 0;
+            comptO = 0;
+            for(j=1;j<=TAILLE;j++){
+                if(grille[i][j]=='X'){
+                    comptX += 1;
+                }
+                if(grille[i][j]=='O'){
+                    comptO += 1;
+                }
+            }
+            if ((comptX == 3)||(comptO == 3)){
+                finish = 1;
+                break;
+            }
+        }
+    }
+
+    //Verification pour le caractere x et o en colonne
+    if(finish == 0){
+        for(j=1;j<=TAILLE;j++){
+            comptX = 0;
+            comptO = 0;
+            for(i=1;i<=TAILLE;i++){
+                if(grille[i][j]=='X'){
+                    comptX += 1;
+                }
+                if(grille[i][j]=='O'){
+                    comptO += 1;
+                }
+            }
+            if ((comptX == 3)||(comptO == 3)){
+                finish = 1;
+                break;
+            }
+        }
+    }
+
+    //Verification des diagonales
+    if((grille[1][1]=='X')&&(grille[2][2]=='X')&&(grille[3][3]=='X')&&(finish == 0)){
+        finish = 1;
+    }
+    if((grille[1][1]=='O')&&(grille[2][2]=='O')&&(grille[3][3]=='O')&&(finish == 0)){
+        finish = 1;
+    }
+    if((grille[1][3]=='X')&&(grille[2][2]=='X')&&(grille[3][1]=='X')&&(finish == 0)){
+        finish = 1;
+    }
+    if((grille[1][3]=='O')&&(grille[2][2]=='O')&&(grille[3][1]=='O')&&(finish == 0)){
+        finish = 1;
+    }
+
+    //Verification de l'egalité
+
+
+    return finish;
+}
+
+//Verification OOB de la coordonée X
+int verifOOBX(int newX){
+    while((newX <1)||(newX > 3)){
+        printf("Donner une valeur de X dans le tableau(1-3)\n");
+        printf("X : ");
+        scanf("%d",&newX);
+        fflush(stdin);
+    }
+    return newX;
+}
+
+//Verification OOB de la coordonée Y
+int verifOOBY(int newY){
+    while((newY <1)||(newY > 3)){
+        printf("Donner une valeur de Y dans le tableau(1-3)\n");
+        printf("Y : ");
+        scanf("%d",&newY);
+        fflush(stdin);
+    }
+    return newY;
+}
+
+/*Verification emplacement deja pris
+int verifOccPlace(char grille[TAILLE][TAILLE], int newX, int newY){
+    int oqp = 0;
+    if(grille[newX][newY] != '_'){
+        printf("La place est deja occupe veuillez en choisir une autre\n");
+        oqp = 1;
+        }
+
+    return oqp;
+}*/
+
